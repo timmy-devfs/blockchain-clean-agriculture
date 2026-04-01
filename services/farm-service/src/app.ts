96726a149b9d7c3106146bac3abaf7713cd8301a
votuannha <nhatvt8667@ut.edu.vn>
2026-04-01 16:51:01 +0700

    [FARM]BICAP-E03- API Quản lý Trang trại & Business License
    # Conflicts:
    #	services/farm-service/package-lock.json

--- services/farm-service/src/app.ts
+++ services/farm-service/src/app.ts
@@ -1,7 +1,9 @@
 import cors from "cors";
 import express from "express";
+import { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
 import { jwtMiddleware } from "./middlewares/jwtMiddleware";
+import { farmRouter } from "./modules/routes/farm.routes";
 import { healthRouter } from "./modules/routes/health.route";
 
 export const app = express();
@@ -12,3 +14,9 @@ app.use(morgan("dev"));
 app.use(jwtMiddleware);
 
 app.use(healthRouter);
+app.use(farmRouter);
+
+app.use((error: unknown, _req: Request, res: Response, _next: NextFunction) => {
+  console.error("[farm-service] Unhandled error", error);
+  res.status(500).json({ message: "Internal Server Error" });
+});
